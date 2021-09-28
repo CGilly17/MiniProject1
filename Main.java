7612778ad980b07c74338eb3491762561f04d127
cgilly17 <89664912+CGilly17@users.noreply.github.com>
2021-09-28 15:31:14 +0000

    added method for playGame and added guessing outputs for user input.

--- Main.java
+++ Main.java
@@ -16,11 +16,47 @@ class Main {
     int limit = s.nextInt();
     Random r = new Random();
     int randomNum = r.nextInt(limit) + 1;
+playGame(limit, randomNum);
+
+
 
   }
-  public static void playGame(int limit, int randomNum)
+  static void playGame(int limit, int goal)
   {
-    
+    Scanner g = new Scanner(System.in);
+    System.out.println("Guess a number between 0 and "+limit);
+    int guess = -1;
+    int tries = 1;
+
+    while (guess != goal)
+    {
+      if(tries>1)
+      {
+        System.out.println("Enter your next guess: ");
+        guess = g.nextInt();
+      }
+      else {
+        System.out.println("Enter your first guess: ");
+        guess = g.nextInt();
+
+      }
+      if(guess == goal)
+      {
+        System.out.println("Great job! This win took you " + tries + " tries!");
+
+      }
+      else if (guess>goal)
+      {
+        System.out.println("Guess a lower number!");
+        tries = tries +1;
+
+      }
+      else {
+        System.out.println("Guess a higher number!");
+        tries = tries +1;
+      }
+    }
+
+
   }
-}
 }
